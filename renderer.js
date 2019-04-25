5110fedc538a56287e931589c6a14a35f0b9aebf
Alkarim Lalani <alkarimlalani@invisionapp.com>
2019-04-25 18:40:33 -0500

    Created child process

--- renderer.js
+++ renderer.js
@@ -1,3 +1,14 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const { fork } = require('child_process');
+
+console.log("Hello from the renderer process!");
+
+ const child = fork('child.js', null, (error, stdout, stderr) => {
+  if (error) {
+    throw error;
+  }
+  console.log(stdout);
+});
